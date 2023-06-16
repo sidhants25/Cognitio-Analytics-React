e48fa36da9e72a52552c036d0881ba3e0b6c1271
sidhants25 <sidhantks@gmail.com>
2023-06-16 10:32:50 -0400

    homepage button

--- CA-React-Form/src/Homepage.jsx
+++ CA-React-Form/src/Homepage.jsx
@@ -8,9 +8,11 @@ import Question from './Question';
 import TextArea from './TextArea';
 import Checkbox from './Checkbox';
 import { Routes, Route } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'
 
 function Homepage() {
-    return (
+    const navigate = useNavigate();
+  return (
       
           <div className="container">
             <h1>
@@ -69,6 +71,11 @@ function Homepage() {
               <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
             </svg>
             } />
+
+            <button 
+            className = "button" onClick={function() {navigate('/submitted');
+          }} > Submit 
+          </button>
   
           </div>
   
